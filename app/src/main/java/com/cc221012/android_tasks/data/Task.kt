67b5902a2b503e23805fb444a01507eb0d3fce3d
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2023-12-04 16:04:12 +0100

    added table name to tasks database

--- app/src/main/java/com/cc221012/android_tasks/data/Task.kt
+++ app/src/main/java/com/cc221012/android_tasks/data/Task.kt
@@ -5,12 +5,13 @@ import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 
 
-@Entity
+@Entity (tableName = "tasks")
 data class Task(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+
     val name: String,
     val description: String?,
     val isCompleted: Boolean = false,
     val dueDate: LocalDateTime? = null,
     val hasReminder: Boolean = false,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
 )
