a91342ae713a8d2d792078688839c02773ee7dbb
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2023-12-04 16:10:10 +0100

    added CRUD function calls to MainViewModel

--- app/src/main/java/com/cc221012/android_tasks/ui/MainViewModel.kt
+++ app/src/main/java/com/cc221012/android_tasks/ui/MainViewModel.kt
@@ -1,14 +1,45 @@
 package com.cc221012.android_tasks.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.cc221012.android_tasks.data.Task
 import com.cc221012.android_tasks.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val dao: TaskDao): ViewModel() {
+
+    // necessary variables for state
     private val _taskState = MutableStateFlow(Task("", null, false, null, false))
     val taskState: StateFlow<Task> = _taskState.asStateFlow()
+
+
+    // CRUD Operations to be called from Dao
+
+    fun createTask(task: Task){
+        viewModelScope.launch {
+            dao.createTask(task)
+        }
+    }
+
+    fun updateTask(task: Task){
+        viewModelScope.launch {
+            dao.updateTask(task)
+        }
+    }
+
+    fun deleteTask(task: Task){
+        viewModelScope.launch {
+            dao.deleteTask(task)
+        }
+    }
+    fun getTasks(){
+        viewModelScope.launch {
+            dao.getTasks()
+        }
+    }
+
 
 }
