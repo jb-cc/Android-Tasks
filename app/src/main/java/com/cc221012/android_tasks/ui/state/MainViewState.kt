a7ea31d1b519be088afee3d432dc0a65b29f1673
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2023-12-05 19:38:25 +0100

    created MainViewState with states

---
+++ app/src/main/java/com/cc221012/android_tasks/ui/state/MainViewState.kt
@@ -0,0 +1,13 @@
+package com.cc221012.android_tasks.ui.state
+
+import com.cc221012.android_tasks.data.Task
+import com.cc221012.android_tasks.ui.views.Tab
+
+class MainViewState {
+    val tasks: List<Task> = emptyList(),
+    val selectedTab: Tab = Tab.CurrentTasks,
+    val newTaskWindowOpened: Boolean = false,
+    val editWindowOpened: Boolean = false,
+    val dueDateWindowOpened: Boolean = false
+
+}
