bee98133d0c3c31d957d69221fe2f98c27d756be
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2024-01-10 03:26:42 +0100

    changed MainViewState from class to data class

--- app/src/main/java/com/cc221012/android_tasks/ui/state/MainViewState.kt
+++ app/src/main/java/com/cc221012/android_tasks/ui/state/MainViewState.kt
@@ -3,11 +3,11 @@ package com.cc221012.android_tasks.ui.state
 import com.cc221012.android_tasks.data.Task
 import com.cc221012.android_tasks.ui.views.Tab
 
-class MainViewState {
+data class MainViewState(
     val tasks: List<Task> = emptyList(),
     val selectedTab: Tab = Tab.CurrentTasks,
     val newTaskWindowOpened: Boolean = false,
     val editWindowOpened: Boolean = false,
     val dueDateWindowOpened: Boolean = false
 
-}
+)
