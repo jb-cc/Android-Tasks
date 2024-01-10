9691427f3aa7daab052b9d1e1cd7b87196b40ab8
Jonas Beer <115902941+jb-cc@users.noreply.github.com>
2024-01-10 04:20:48 +0100

    added mainViewModel to Activity and display it

--- app/src/main/java/com/cc221012/android_tasks/MainActivity.kt
+++ app/src/main/java/com/cc221012/android_tasks/MainActivity.kt
@@ -3,51 +3,48 @@ package com.cc221012.android_tasks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.cc221012.android_tasks.data.TasksDatabase
-import com.cc221012.android_tasks.ui.theme.AndroidTasksTheme
+import com.cc221012.android_tasks.ui.viewModels.MainViewModel
+import com.cc221012.android_tasks.ui.views.MainView
 
 class MainActivity : ComponentActivity() {
     private val db by lazy {
         Room.databaseBuilder(this, TasksDatabase::class.java, "TasksDatabase.db").build()
     }
 
+    private val mainViewModel by viewModels<MainViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory{
+                override fun <T : ViewModel> create(modelClass: Class<T>): T{
+                    return MainViewModel(db.dao) as T
+                }
+            }
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AndroidTasksTheme {
+            MaterialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val mainViewState by mainViewModel.tasksListState.collectAsState()
+                    MainView(mainViewState)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidTasksTheme {
-        Greeting("Android")
     }
 }
